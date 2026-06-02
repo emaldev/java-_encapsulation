beae262cd1264af275d0341fabbc4b8290f538fd
emaldev <261611436+emaldev@users.noreply.github.com>
2026-06-03 00:33:12 +0430

    Add Student and School encapsulation example

---
+++ src/School.java
@@ -0,0 +1,11 @@
+public class School {
+    public static void main(String[] args){
+
+        Student student = new Student();
+         student.setName("Ali");
+         student.setAge(21);
+        System.out.println("The Student name is : " + student.getName());
+        System.out.println("This is the student Age : " + student.getAge());
+    }
+
+}
---
+++ src/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    private String name;
+    private int age;
+
+    public String  getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setName(String newname){
+        this.name = newname;
+    }
+    public void setAge(int newage){
+        this.age = newage;
+    }
+
+}
