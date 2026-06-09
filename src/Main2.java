d9d181e64bf0f3cbb6fc3315c03b071d63053ea4
emaldev <261611436+emaldev@users.noreply.github.com>
2026-06-10 00:35:33 +0430

    Student Information Encapsulation and Constructor

---
+++ src/Main2.java
@@ -0,0 +1,10 @@
+public class Main2 {
+    public static void main(String[] args){
+        Student2 studentInforamtion = new Student2("Ali Mohammadi ");
+        studentInforamtion.setGrade(25);
+        studentInforamtion.setStudentId("893412");
+        studentInforamtion.desplayInfo();
+
+
+    }
+}
