aa0d76be07ba7c22d451f7a524fe940c8bfb9955
emaldev <261611436+emaldev@users.noreply.github.com>
2026-06-09 13:54:24 +0430

    java Encapsulation practise

---
+++ src/Book.java
@@ -0,0 +1,9 @@
+public class Book {
+    public static void main(String[] args){
+
+        Book_name book2 = new Book_name();
+           book2.setName("Mohammad");
+        System.out.println(book2.getName());
+
+    }
+}
